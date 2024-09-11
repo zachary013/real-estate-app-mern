0b74fa24224b1bb80ae48b42ee195db8c3c0965f
Zakariae Azn <azarkanzakariae@gmail.com>
2024-09-11 23:17:40 +0100

    Prisma db

--- api/app.js
+++ api/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 
 const app = express();
 
+//lets us use json data
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 
 app.listen(8800, () => {
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,50 @@
+import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
 
-export const register = (req, res) => {
-    res.send('register');
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            }
+        });
+        console.log(newUser);
+        res.status(201).json({ message: 'User created' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Failed to create user' });
+    }
 }
 
-export const login = (req, res) => {
-    res.send('login');
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        //? Check if user exists
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        //? Check if user exists
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Failed to login' });
+    }
+
 }
 
 export const logout = (req, res) => {
-    res.send('logout');
+    console.log('logout');
 }
