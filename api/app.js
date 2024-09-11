18e718503e35cdb3e81f96799789066643ee2e30
Zakariae Azn <azarkanzakariae@gmail.com>
2024-09-11 21:51:52 +0100

    Create controllers

--- api/app.js
+++ api/app.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import postRoutes from './routes/post.route.js';
 
 const app = express();
 
-app.use('/api/test', (req, res) => {
-    res.send('Hello World');
-})
+app.use('/api/posts', postRoutes);
 
 app.listen(8800, () => {
     console.log('Server is running on port 8800');
---
+++ api/routes/post.route.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const router = express.Router();
+
+// router.get('/test', (req, res) => {
+//     res.send('Hello Bitches');
+// })
+
+// router.post('/test', (req, res) => {
+//     res.send('Hello Bitches');
+// })
+
+// router.put('/test', (req, res) => {
+//     res.send('Hello Bitches');
+// })
+
+// router.delete('/test', (req, res) => {
+//     res.send('Hello Bitches');
+// })
+
+export default router;
