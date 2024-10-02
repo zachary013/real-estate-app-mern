d4a6502f64c3cdc0eb2dc4a8728ff37c68e09715
Zakariae Azn <azarkanzakariae@gmail.com>
2024-10-03 00:23:03 +0100

    User Routes

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
+import authRoute from './routes/auth.route.js';
+import postRoute from './routes/post.route.js';
 import testRoute from './routes/test.route.js';
+import userRoute from './routes/user.route.js';
 
 const app = express();
 
@@ -13,8 +14,9 @@ app.use(express.json());
 //use cookies
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 app.use('/api/test', testRoute);
 
 const PORT = process.env.PORT || 8800;
---
+++ api/controllers/user.controller.js
@@ -0,0 +1,39 @@
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to get Users"});
+    }
+}
+
+export const getUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to get User"});
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to update User"});
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: "Failed to delete User"});
+    }
+}
