96f139917d74e9fd79b5233c09cc997e3e3e33f8
Zakariae Azn <azarkanzakariae@gmail.com>
2024-10-09 15:53:02 +0100

    Post Routes

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,21 +1,13 @@
-import express from 'express';
+import express from "express";
+import {verifyToken} from "../middleware/verifyToken.js";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-// router.get('/test', (req, res) => {
-//     res.send('Hello Bitches');
-// })
-
-// router.post('/test', (req, res) => {
-//     res.send('Hello Bitches');
-// })
-
-// router.put('/test', (req, res) => {
-//     res.send('Hello Bitches');
-// })
-
-// router.delete('/test', (req, res) => {
-//     res.send('Hello Bitches');
-// })
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken, deletePost);
 
 export default router;
