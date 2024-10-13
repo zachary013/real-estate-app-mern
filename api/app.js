80a5376ce6ac0ec03ac5ecab83583cd5abfb576e
Zakariae Azn <azarkanzakariae@gmail.com>
2024-10-13 22:50:36 +0100

    Initial Chat Integration

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ import authRoute from './routes/auth.route.js';
 import postRoute from './routes/post.route.js';
 import testRoute from './routes/test.route.js';
 import userRoute from './routes/user.route.js';
+import chatRoute from './routes/chat.route.js';
+import messageRoute from './routes/message.route.js';
 
 const app = express();
 
@@ -18,6 +20,9 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/test', testRoute);
+app.use('/api/chats', chatRoute);
+app.use('/api/messages', messageRoute);
+
 
 const PORT = process.env.PORT || 8800;
 
